8927c07c5b04b475ea8bbfb283709ba958ebd1f7
Alex <alex.fr.estudio@gmail.com>
2025-06-23 15:07:55 +0100

    small fix

--- src/components/Tabs.tsx
+++ src/components/Tabs.tsx
@@ -7,29 +7,39 @@ interface TabProps {
 }
 
 const TabsContainer = styled.div`
-    display: flex;
-    flex-direction: row;
-    border-radius: 30px;
-    padding: 3px;
-    background-color: rgba(0, 0, 0, 0.6);
-    backdrop-filter: blur(5px);
-    @media (max-width: 769px) {
-        width:90%;
-    }
+  display: flex;
+  flex-direction: row;
+  border-radius: 30px;
+  padding: 3px;
+  background-color: rgba(0, 0, 0, 0.6);
+  backdrop-filter: blur(5px);
+  overflow-x: auto;
+  max-width: 100%;
+  box-sizing: border-box;
+  margin: 10px;
+  @media (max-width: 769px) {
+    width: 100%;
+    justify-content: center;
+    align-items: center;
+  }
 `;
 
+
 const TabLink = styled.a`
-    font-size: 1.2em;
-    padding: 10px 20px;
-    cursor: pointer;
-    color: white;
-    text-decoration: none;
-    white-space: nowrap;
-    @media (max-width: 769px) {
-        font-size: 1em;
-        padding: 8px 16px;
-    }
+  font-size: 1.2em;
+  padding: 10px 20px;
+  cursor: pointer;
+  color: white;
+  text-decoration: none;
+  white-space: nowrap;
+  box-sizing: border-box;
+  
+  @media (max-width: 769px) {
+    font-size: 1em;
+    padding: 6px 12px; // reducido aún más
+  }
 `;
+
 
 const Tabs: React.FC<TabProps> = ({ labels, onClick }) => {
     return (
